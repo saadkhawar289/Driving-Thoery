60b10a9b4436d9e861dd5cc0d3d9a2659a10a897
Gomal <ishfaqahmadia789@gmail.com>
2021-10-25 17:40:35 +0500

    3UI

--- driving_theory/lib/main.dart
+++ driving_theory/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-import 'Screens/road_signs.dart';
+import 'Screens/practice.dart';
 
 void main() {
   runApp(MyApp());
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
                   theme: ThemeData(primaryColor: const Color(0xFF404FFA)),
                   // home: Splash(),
                   routes: {
-                    '/': (BuildContext context) => RoadSigns(),
+                    '/': (BuildContext context) => Practice(),
                   },
                 )));
   }
